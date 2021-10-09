a45c300ee4223ee89e872f3288bcea8406e0e826
tracer-nju <tracer@nju.edu.cn>
2021-10-09 21:26:49 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: fba254ef40b34b41a69cc353e81910ed     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:26:50 up 16 min,  1 user,  load average: 0.97, 0.66, 0.38

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-static inline int carry(uint64_t a, uint64_t b) {
+static inline int is_overflow(uint64_t a, uint64_t b) {
     int c = 0;
     for (int i = 0; i < 64; ++ i, a >>= 1, b >>= 1){
         c = ((a & 1) & (b & 1)) | ((a & 1) & (c & 1)) | ((c & 1) & (b & 1));
@@ -10,17 +10,17 @@ static inline int carry(uint64_t a, uint64_t b) {
 
 static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
     //wait to be solved
-    /*
-    if (1) {
+    if (is_overflow(a, b)) {
         uint64_t t = 0xffffffffffffffff - a;
         b = b - t;
         a = 0xffffffffffffffff;
     }
     else {
         a = a + b;
+
     }
     bool flag = false;
-    */
+    
     return (a + b) % m;
 }
 
