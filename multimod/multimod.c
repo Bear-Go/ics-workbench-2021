12e51c5e31c300261e4d5efb78777b61929f9e88
tracer-nju <tracer@nju.edu.cn>
2021-10-10 01:50:49 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: fba254ef40b34b41a69cc353e81910ed     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  01:50:49 up  3:15,  1 user,  load average: 0.26, 0.19, 0.15

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
+#include <assert.h>
 
 static inline int is_overflow(uint64_t a, uint64_t b) {
     int c = 0;
     for (int i = 0; i < 64; ++ i, a >>= 1, b >>= 1){
         c = ((a & 1) & (b & 1)) | ((a & 1) & (c & 1)) | ((c & 1) & (b & 1));
     }
+    assert(c == 0 || c == 1);
     return c;
 }
 
@@ -12,6 +14,8 @@ static inline void transfer(uint64_t *a, uint64_t *b) {
     uint64_t t = 0xffffffffffffffff - *a;
     *b = *b - t;
     *a = 0xffffffffffffffff;
+    assert(*a);
+    assert(*b);
     return;
 }
 
@@ -25,10 +29,13 @@ static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
     while (b >= m && b > 0){
         b -= m;
     }
+    assert(a);
+    assert(b);
     a += b;
     while (a >= m && a > 0) {
         a -= m;
     }
+    assert(a);
     return a;
 }
 
@@ -40,5 +47,6 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
         }
         b = addmod(b, b, m);
     }
+    assert(ans);
     return ans;
 }
