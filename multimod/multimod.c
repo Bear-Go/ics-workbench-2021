d5ed0cdd726aca22f33e6dcacaf85c8cd3d66a3d
tracer-nju <tracer@nju.edu.cn>
2021-10-09 21:23:10 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: fba254ef40b34b41a69cc353e81910ed     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:23:10 up 12 min,  1 user,  load average: 1.37, 0.49, 0.27

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,36 @@
 #include <stdint.h>
 
+static inline int carry(uint64_t a, uint64_t b) {
+    int c = 0;
+    for (int i = 0; i < 64; ++ i, a >>= 1, b >>= 1){
+        c = ((a & 1) & (b & 1)) | ((a & 1) & (c & 1)) | ((c & 1) & (b & 1));
+    }
+    return c;
+}
+
+static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
+    //wait to be solved
+    if (c) {
+        uint64_t t = 0xffffffffffffffff - a;
+        b = b - t;
+        a = 0xffffffffffffffff;
+    }
+    else {
+        a = a + b;
+    }
+    bool flag = false;
+    return (a + b) % m;
+}
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+    uint64_t old = (a * b) % m,ans = 0;
+    for (; a != 0; a >>= 1) {
+        if (a & 1) {
+            ans = addmod(ans, b, m);
+        }
+        b = addmod(b, b, m);
+        printf(U64 " " U64 "\n", a, b);
+    }
+    if (old != ans) printf("old != ans\n");
+    return ans;
 }
