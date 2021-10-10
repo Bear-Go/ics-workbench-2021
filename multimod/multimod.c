ef5dc415d6e6bf748440813fda123240a3e7ebb7
tracer-nju <tracer@nju.edu.cn>
2021-10-10 12:16:25 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: fba254ef40b34b41a69cc353e81910ed     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  12:16:25 up 13:40,  1 user,  load average: 1.12, 0.58, 0.43

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,15 +1,10 @@
 #include <stdint.h>
-#include <assert.h>
 
 static inline uint64_t mod(uint64_t a, uint64_t m) {
     uint64_t b = m;
-    while (b <= a >> 1) {
-        b <<= 1;
-    }
+    while (b <= a >> 1) b <<= 1;
     while (a >= m) {
-        if (a >= b) {
-            a -= b;
-        }
+        if (a >= b) a -= b;
         b >>= 1;
     }
     return a;
@@ -17,22 +12,17 @@ static inline uint64_t mod(uint64_t a, uint64_t m) {
 
 static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
     uint64_t max = 0xffffffffffffffff;
-    if (a + b < a || a + b < b) {
+    if (a + b < a || a + b < b)
         return addmod(mod(a + b, m), mod(max, m) + 1, m);
-    }
-    else {
+    else
         return mod(a + b, m);
-    }
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
     uint64_t ans = 0;
     for (; a ; a >>= 1) {
-        if (a & 1) {
-            ans = addmod(ans, b, m);
-        }
+        if (a & 1) ans = addmod(ans, b, m);
         b = addmod(b, b, m);
     }
-    assert(ans>=0);
     return ans;
 }
