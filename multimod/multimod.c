0a3710c3e3ba5e9910164dc6d6756edc2f19ed84
tracer-nju <tracer@nju.edu.cn>
2021-10-10 10:51:27 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: fba254ef40b34b41a69cc353e81910ed     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  10:51:27 up 12:16,  1 user,  load average: 0.23, 0.22, 0.16

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,52 +1,76 @@
 #include <stdint.h>
 #include <assert.h>
 
-static inline int is_overflow(uint64_t a, uint64_t b) {
-    int c = 0;
-    for (int i = 0; i < 64; ++ i, a >>= 1, b >>= 1){
-        c = ((a & 1) & (b & 1)) | ((a & 1) & (c & 1)) | ((c & 1) & (b & 1));
-    }
-    assert(c == 0 || c == 1);
-    return c;
-}
+// static inline int is_overflow(uint64_t a, uint64_t b) {
+//     int c = 0;
+//     for (int i = 0; i < 64; ++ i, a >>= 1, b >>= 1){
+//         c = ((a & 1) & (b & 1)) | ((a & 1) & (c & 1)) | ((c & 1) & (b & 1));
+//     }
+//     assert(c == 0 || c == 1);
+//     return c;
+// }
 
-static inline void transfer(uint64_t *a, uint64_t *b) {
-    uint64_t t = 0xffffffffffffffff - *a;
-    *b = *b - t;
-    *a = 0xffffffffffffffff;
-    assert(*a);
-    assert(*b);
-    return;
-}
+// static inline void transfer(uint64_t *a, uint64_t *b) {
+//     uint64_t t = 0xffffffffffffffff - *a;
+//     *b = *b - t;
+//     *a = 0xffffffffffffffff;
+//     assert(*a);
+//     assert(*b);
+//     return;
+// }
 
-static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
-    if (is_overflow(a, b)) {
-        transfer(&a, &b);
+// static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
+//     if (is_overflow(a, b)) {
+//         transfer(&a, &b);
+//     }
+//     while (a >= m && a > 0) {
+//         a -= m;
+//     }
+//     while (b >= m && b > 0){
+//         b -= m;
+//     }
+//     assert(a>=0);
+//     assert(b>=0);
+//     a += b;
+//     while (a >= m && a > 0) {
+//         a -= m;
+//     }
+//     assert(a>=0);
+//     return a;
+// }
+
+// uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+//     uint64_t ans = 0;
+//     for (; a >  0; a >>= 1) {
+//         if (a & 1) {
+//             ans = addmod(ans, b, m);
+//         }
+//         b = addmod(b, b, m);
+//     }
+//     assert(ans>=0);
+//     return ans;
+// }
+
+uint64_t mod(uint64_t x, uint64_t m) {
+    assert(x >= 0);
+    while (x >= m) {
+        x -= m;
     }
-    while (a >= m && a > 0) {
-        a -= m;
-    }
-    while (b >= m && b > 0){
-        b -= m;
-    }
-    assert(a>=0);
-    assert(b>=0);
-    a += b;
-    while (a >= m && a > 0) {
-        a -= m;
-    }
-    assert(a>=0);
-    return a;
+    assert(x >= 0);
+    return x;
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-    uint64_t ans = 0;
-    for (; a >  0; a >>= 1) {
+    uint64_t res = 0;
+    b = mod(b, m);
+    while (a) {
         if (a & 1) {
-            ans = addmod(ans, b, m);
+            res += b;
+            res = mod(res, m);
         }
-        b = addmod(b, b, m);
+        b += b;
+        b = mod(b, m);
+        a >>= 1;
     }
-    assert(ans>=0);
-    return ans;
+    return res;
 }
