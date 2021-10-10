d0eaeb928787121f73aa7a9b704061061353d0d5
tracer-nju <tracer@nju.edu.cn>
2021-10-10 11:17:11 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: fba254ef40b34b41a69cc353e81910ed     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  11:17:11 up 12:41,  1 user,  load average: 0.24, 0.26, 0.21

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,80 +1,103 @@
 #include <stdint.h>
 #include <assert.h>
 
-// static inline int is_overflow(uint64_t a, uint64_t b) {
-//     int c = 0;
-//     for (int i = 0; i < 64; ++ i, a >>= 1, b >>= 1){
-//         c = ((a & 1) & (b & 1)) | ((a & 1) & (c & 1)) | ((c & 1) & (b & 1));
-//     }
-//     assert(c == 0 || c == 1);
-//     return c;
-// }
+static inline int is_overflow(uint64_t a, uint64_t b) {
+    int c = 0;
+    for (int i = 0; i < 64; ++ i, a >>= 1, b >>= 1){
+        c = ((a & 1) & (b & 1)) | ((a & 1) & (c & 1)) | ((c & 1) & (b & 1));
+    }
+    assert(c == 0 || c == 1);
+    return c;
+}
 
-// static inline void transfer(uint64_t *a, uint64_t *b) {
-//     uint64_t t = 0xffffffffffffffff - *a;
-//     *b = *b - t;
-//     *a = 0xffffffffffffffff;
-//     assert(*a);
-//     assert(*b);
-//     return;
-// }
+static inline void transfer(uint64_t *a, uint64_t *b) {
+    uint64_t t = 0xffffffffffffffff - *a;
+    *b = *b - t;
+    *a = 0xffffffffffffffff;
+    assert(*a);
+    assert(*b);
+    return;
+}
 
-// static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
-//     if (is_overflow(a, b)) {
-//         transfer(&a, &b);
-//     }
-//     while (a >= m && a > 0) {
-//         a -= m;
-//     }
-//     while (b >= m && b > 0){
-//         b -= m;
-//     }
-//     assert(a>=0);
-//     assert(b>=0);
-//     a += b;
-//     while (a >= m && a > 0) {
-//         a -= m;
-//     }
-//     assert(a>=0);
-//     return a;
-// }
-
-// uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-//     uint64_t ans = 0;
-//     for (; a >  0; a >>= 1) {
-//         if (a & 1) {
-//             ans = addmod(ans, b, m);
-//         }
-//         b = addmod(b, b, m);
-//     }
-//     assert(ans>=0);
-//     return ans;
-// }
+static inline uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
+    if (is_overflow(a, b)) {
+        transfer(&a, &b);
+    }
+    if (a >= m) {
+        a -= m;
+    }
+    if (b >= m) {
+        b -= m;
+    }
+    assert(a>=0);
+    assert(b>=0);
+    a += b;
+    if (a >= m) {
+        a -= m;
+    }
+    assert(a>=0);
+    return a;
+}
 
 uint64_t mod(uint64_t x, uint64_t m) {
-    assert(x >= 0);
-    while (x >= m) {
-        x -= m;
+    if (x < m) {
+        return x;
     }
-    assert(x >= 0);
-    return x;
+    
+    uint64_t k, c, quotient = 0, remainder;
+    while (x > m) {
+        for (k = 0, c = m; x >= c; c <<= 1, ++ k) {
+            if (x - c < m) {
+                quotient += 1 << k;
+                remainder = x - c;
+                break;
+            }
+        }
+        if (x - c < m) {
+            break;
+        }
+        quotient += 1 << (k -1);
+        x -= c >> 1;
+    }
+    return remainder;
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-    uint64_t res = 0;
-    b = mod(b, m);
-    while (a) {
+    uint64_t ans = 0;
+    for (b = mod(b, m); a ; a >>= 1) {
         if (a & 1) {
-            res += b;
-            if (res >= m) {
-                res -= m;
-            }
+            ans = addmod(ans, b, m);
         }
-        b += b;
-        if (b >= m) {
-            b -= m;
-        }
-        a >>= 1;
+        b = addmod(b, b, m);
     }
-    return res;
+    assert(ans>=0);
+    return ans;
 }
+
+// uint64_t mod(uint64_t x, uint64_t m) {
+//     assert(x >= 0);
+//     while (x >= m) {
+//         x -= m;
+//     }
+//     assert(x >= 0);
+//     return x;
+// }
+
+// uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+//     uint64_t res = 0;
+//     b = mod(b, m);
+//     while (a) {
+//         if (a & 1) {
+//             res += b;
+//             if (res >= m) {
+//                 res -= m;
+//             }
+//         }
+//         b += b;
+//         if (b >= m) {
+//             b -= m;
+//         }
+//         a >>= 1;
+//     }
+//     return res;
+// }
