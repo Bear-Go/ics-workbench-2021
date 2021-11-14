aa5ec664edf97072d22b7032b47687b8858c6683
tracer-nju <tracer@nju.edu.cn>
2021-11-14 18:37:19 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: c8c4c554174d4cc8ac8425bc8a176b67     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  18:37:19 up 16 min,  1 user,  load average: 0.33, 0.46, 0.36

--- asm/main.c
+++ asm/main.c
@@ -4,13 +4,14 @@
 
 int main() {
   asm_jmp_buf buf;
-  int r = setjmp(buf);
+  int r = 0;
+  // int r = setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     printf("Add & Popcnt done.\n");
     // TODO: add more tests here.
-    longjmp(buf, 123);
+    // longjmp(buf, 123);
   } else {
     assert(r == 123);
     printf("PASSED.\n");
