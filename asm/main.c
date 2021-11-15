4aad1512fef3aa54d19eddce8e60bbe5d2fdfbdb
tracer-nju <tracer@nju.edu.cn>
2021-11-15 20:21:26 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: c8c4c554174d4cc8ac8425bc8a176b67     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:21:26 up 13:44,  1 user,  load average: 0.26, 0.24, 0.22

--- asm/main.c
+++ asm/main.c
@@ -8,7 +8,7 @@ int main() {
   // int r = setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
-    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+    assert(asm_popcnt(13) == 3);
     char test[10];
     char s[]="hello!";
     asm_memcpy(test, s, 6);
