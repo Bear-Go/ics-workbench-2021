8147b90186fff8ba9ff72ec993408854fbe7629f
tracer-nju <tracer@nju.edu.cn>
2021-11-15 21:52:37 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: c8c4c554174d4cc8ac8425bc8a176b67     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  21:52:37 up 15:15,  1 user,  load average: 0.51, 0.33, 0.29

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -3,7 +3,7 @@
 
 int64_t asm_add(int64_t a, int64_t b) {
   asm(
-    "addq %%rbx, %%rax\n\t"
+    "addq %%rbx, %%rax\n\t" // simple
     : "+a"(a)
     : "b"(b)
     );
@@ -12,7 +12,16 @@ int64_t asm_add(int64_t a, int64_t b) {
  
 int asm_popcnt(uint64_t x) {
   int cnt = 0;
-  asm(
+  asm( 
+    /*  
+    * Obeserve: gcc may do something to make up
+    * for the missing part between asm & c language
+    * Example: 
+    * When using a function it seems that both writing
+    * and not writing the push part in asm are correct.
+    * so what happens during this procedure 
+    * maybe a clearer asm coding style is needed
+    */
     "movl $0, %%eax\n\t" // ? local: rm wrong/oj: rm right 
     "movl $0, %%ecx\n\t"
     "inside:\n\t"
@@ -34,7 +43,7 @@ int asm_popcnt(uint64_t x) {
 void *asm_memcpy(void *dest, const void *src, size_t n) {
   void *ret = NULL;
   asm(
-    "movq %[dest],  %[ret]\n\t" // hard
+    "movq %[dest],  %[ret]\n\t" // hard version
     "rep movsb\n\t"
     : [dest] "+D"(dest), [ret] "+g"(ret)
     : "c"(n), "S"(src)
@@ -46,7 +55,7 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 int asm_setjmp(asm_jmp_buf env) {
   asm(
     "mov  %[env], %%rax\n\t"
-    "mov  %%rbx, (%%rax)\n\t"
+    "mov  %%rbx, (%%rax)\n\t" // save the env %rbc %rsi %rdi %rbp %rsp+8 %rsp
     "mov  %%rsi, 8(%%rax)\n\t"
     "mov  %%rdi, 16(%%rax)\n\t"
     "mov  %%rbp, 24(%%rax)\n\t"
@@ -62,8 +71,15 @@ int asm_setjmp(asm_jmp_buf env) {
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
-  asm(
-    "mov  %[env], %%rdx\n\t"
+  /*
+  * When trying to get the meaning of setjmp & longjmp
+  * I find that these two function cannot be packaged into
+  * another two functions after RTFW I got a possible ans
+  * that is the package will disrupt the order of the exc
+  * and this leads to the error of longjmp
+  */
+  asm( // naive version : no signal mask & not check val ?= 0 
+    "mov  %[env], %%rdx\n\t"  // backto the env & change the %eax to val
     "mov  %[val], %%eax\n\t"
     "mov  (%%rdx),  %%rbx\n\t"
     "mov  8(%%rdx), %%rsi\n\t"
