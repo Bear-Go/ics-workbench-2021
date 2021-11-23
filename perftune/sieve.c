505de2a0725658316ebfe6ab220874355b08023b
tracer-nju <tracer@nju.edu.cn>
2021-11-24 01:02:18 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: cc2f5a92e4bc4204b35449bc4773ec5e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  01:02:18 up  1:00,  1 user,  load average: 0.66, 0.74, 0.54

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -12,13 +12,13 @@ static int  primes[N];
 int *sieve(int n) {
   primes[0] = 2;
   int cnt = 1;
-  int i = 0, num = (i << 2) + 3;
-  for (; num < n; num = ++i * 2 + 3) {
+  for (int i = 0, num = 3; num < n; num = ++i * 2 + 3) {
     if (!not_prime[i]) primes[cnt++] = num;
     for (int j = 1; j < cnt && num * primes[j] < n; ++j) {
       not_prime[(num * primes[j] - 3) / 2] = 1;
       if (num % primes[j] == 0) break;
     }
+    num = ((++i) << 1) + 3;
   }
   return primes;
 }
