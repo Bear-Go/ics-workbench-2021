d737696ac75b38e7e7a9f89f3bf1ed81f3d86fff
tracer-nju <tracer@nju.edu.cn>
2021-12-07 00:06:47 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 89c39991e9bf47c9b7dec9fc1842fc03     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  00:06:47 up 1 day,  8:50,  1 user,  load average: 0.22, 0.56, 0.44

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -10,13 +10,21 @@ void cycle_increase(int n) { cycle_cnt += n; }
 
 // TODO: implement the following functions
 
+// 从 cache 中读出 addr 地址处的 4 字节数据
+// 若缺失，需要先从内存中读入数据
 uint32_t cache_read(uintptr_t addr) {
   return 0;
 }
 
+// 往 cache 中 addr 地址所属的块写入数据 data，写掩码为 wmask
+// 例如当 wmask 为 0xff 时，只写入低8比特
+// 若缺失，需要从先内存中读入数据
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 }
 
+// 初始化一个数据大小为 2^total_size_width B，关联度为 2^associativity_width 的 cache
+// 例如 init_cache(14, 2) 将初始化一个 16KB，4 路组相联的cache
+// 将所有 valid bit 置为无效即可
 void init_cache(int total_size_width, int associativity_width) {
 }
 
