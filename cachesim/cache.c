104a75a2969e4185c8fceef6deb3d6bd9e60b7c4
tracer-nju <tracer@nju.edu.cn>
2021-12-07 01:21:45 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 89c39991e9bf47c9b7dec9fc1842fc03     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  01:21:45 up 1 day, 10:05,  1 user,  load average: 0.27, 0.36, 0.33

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -11,8 +11,9 @@ void cycle_increase(int n) { cycle_cnt += n; }
 // TODO: implement the following functions
 
 #define CACHE_SIZE (1 << 20) // 1MB
-
+#define BLOCK_NUM_MAX exp2(CACHE_SIZE / BLOCK_SIZE)
 static uint8_t cache[CACHE_SIZE];
+static bool valid_bit[BLOCK_NUM_MAX];
 static uint32_t BLOCK_NUM = 0;
 static uint32_t SET_NUM = 0;
 static uint32_t SET_WIDTH = 0;
