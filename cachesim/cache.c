f919478780673e654a41f12f343dc323bfc4eb0c
tracer-nju <tracer@nju.edu.cn>
2021-12-08 03:19:57 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 89c39991e9bf47c9b7dec9fc1842fc03     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  03:19:58 up 1 day, 21:25,  1 user,  load average: 0.43, 0.36, 0.22

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -73,7 +73,7 @@ uint32_t cache_read(uintptr_t addr) {
       uint32_t *ret = (uint32_t *)(this_cache[i].data + addr_in_block); // duo du le liang ge zi jie
       uint8_t *t = (uint8_t *)ret;
       printf("0x%02x\n", *t);
-      printf("0x%02x\n", *(t+2));
+      printf("0x%02x\n", *(t-2));
       printf("exist invalid bit\n");
       printf("cache read : 0x%08x\n", *ret);
       return *ret;
