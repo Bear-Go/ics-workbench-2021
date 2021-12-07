7c36868e50e3fe59a1d9945111d03480f209e35d
tracer-nju <tracer@nju.edu.cn>
2021-12-08 02:05:06 +0800

    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 89c39991e9bf47c9b7dec9fc1842fc03     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  02:05:06 up 1 day, 20:10,  1 user,  load average: 0.29, 0.34, 0.27

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -40,6 +40,9 @@ uint32_t cache_read(uintptr_t addr) {
   uint32_t addr_in_block = ADDR_IN_BLOCK(addr); // addr in block
   line *this_cache = &cache[SET_SIZE * index];
 
+  printf("addr : 0x%08x\n", addr);
+  printf("index of set : 0x%08x", index);
+
   // check whether hit
   for (int i = 0; i < SET_SIZE; ++ i) {
     // hit
